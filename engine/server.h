eca3c4ce8923b60e95b77fddf281f85cc7f51aa6
Saint Wish <SaintWish@users.noreply.github.com>
2023-10-19 13:22:40 -0600

    Added cvarhook from latest HLDS build
    From https://github.com/dreamstalker/rehlds/commit/6f031901cfa85d86c6028086bc9335db1e867a03

--- engine/server.h
+++ engine/server.h
@@ -350,7 +350,6 @@ extern cvar_t sv_crashcfg;
 extern cvar_t sv_outofdatetime;
 extern cvar_t mapchangecfgfile;
 
-extern qboolean allow_cheats;
 extern cvar_t mp_logecho;
 extern cvar_t mp_logfile;
 extern cvar_t sv_allow_download;
