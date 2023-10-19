6692149ded498f5cc94d9d38c4a06df016a10b2b
Saint Wish <SaintWish@users.noreply.github.com>
2023-10-19 13:46:27 -0600

    Add cheat command noclip
    From https://github.com/dreamstalker/rehlds/commit/7fcec97af4c6598ab6a65d60cf424b7775a11729

--- engine/host_cmd.cpp
+++ engine/host_cmd.cpp
@@ -660,6 +660,73 @@ void Host_Status_f(void)
 	Host_Status_Printf(conprint, log, "%i users\n", nClients);
 }
 
+// Searches along the direction ray in steps of "step" to see if
+// the entity position is passible
+// Used for putting the player in valid space when toggling off noclip mode
+int FindPassableSpace(edict_t *pEdict, vec_t *direction, float step)
+{
+	int		i;
+
+	for (i = 0; i < 100; i++)
+	{
+		VectorMA(pEdict->v.origin, step, direction, pEdict->v.origin);
+
+		if (!SV_TestEntityPosition(pEdict))
+		{
+			// Store old origin
+			VectorCopy(pEdict->v.origin, pEdict->v.oldorigin);
+			return TRUE;
+		}
+	}
+
+	return FALSE;
+}
+
+void Host_Noclip_f(void)
+{
+	if (cmd_source == src_command)
+	{
+		Cmd_ForwardToServer();
+		return;
+	}
+
+	if (!sv_cheats.value)
+		return;
+
+	if (sv_player->v.movetype != MOVETYPE_NOCLIP)
+	{
+		sv_player->v.movetype = MOVETYPE_NOCLIP;
+		SV_ClientPrintf("noclip ON\n");
+	}
+	else
+	{
+		sv_player->v.movetype = MOVETYPE_WALK;
+
+		// Store old origin
+		VectorCopy(sv_player->v.origin, sv_player->v.oldorigin);
+
+		SV_ClientPrintf("noclip OFF\n");
+
+		if (SV_TestEntityPosition(sv_player))
+		{
+			vec3_t forward, right, up;
+			AngleVectors(sv_player->v.v_angle, forward, right, up);
+
+			if (!FindPassableSpace(sv_player, forward, 1.0)
+				&& !FindPassableSpace(sv_player, right, 1.0)
+				&& !FindPassableSpace(sv_player, right, -1.0)		// left
+				&& !FindPassableSpace(sv_player, up, 1.0)			// up
+				&& !FindPassableSpace(sv_player, up, -1.0)			// down
+				&& !FindPassableSpace(sv_player, forward, -1.0))	// back
+			{
+				Con_DPrintf("Can't find the world\n");
+			}
+
+			VectorCopy(sv_player->v.oldorigin, sv_player->v.origin);
+		}
+	}
+}
+
 void Host_Status_Formatted_f(void)
 {
 	client_t *client;
@@ -3136,12 +3203,13 @@ void Host_InitCommands(void)
 	Cmd_AddCommand("stopdemo", Host_Stopdemo_f);
 	Cmd_AddCommand("setinfo", Host_SetInfo_f);
 	Cmd_AddCommand("fullinfo", Host_FullInfo_f);
+	
+	Cmd_AddCommand("noclip", Host_Noclip_f);
 
 #ifndef SWDS
 	Cmd_AddCommand("god", Host_God_f);
 	Cmd_AddCommand("notarget", Host_Notarget_f);
 	Cmd_AddCommand("fly", Host_Fly_f);
-	Cmd_AddCommand("noclip", Host_Noclip_f);
 	Cmd_AddCommand("viewmodel", Host_Viewmodel_f);
 	Cmd_AddCommand("viewframe", Host_Viewframe_f);
 	Cmd_AddCommand("viewnext", Host_Viewnext_f);
